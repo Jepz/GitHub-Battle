e1c4c0e4c077d6e1eedb28dd119109f9b273f3d1
jepz <jesper.lindholmer@avaloninnovation.com>
2017-08-03 12:50:03 +0200

    added ny file, loading js

---
+++ app/components/Loading.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var PropTypes = require("prop-types");
+
+class Loading extends React.Component {
+  render() {
+    return <h1>hello world</h1>;
+  }
+}
+
+module.exports = Loading;
